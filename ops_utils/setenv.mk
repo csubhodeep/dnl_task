faaad67f8663cab6a822d2aea22276301e2ba037
csubhodeep <buks1019@gmail.com>
2023-09-12 23:06:13 +0200

    fix paths

--- ops_utils/setenv.mk
+++ ops_utils/setenv.mk
@@ -2,12 +2,12 @@ all: install_deps
 
 create_virtualenv:
 	@echo "Creating virtualenv"
-	@python3 -m venv ../venv --clear
+	@python3 -m venv ./venv --clear
 
 update_pip: create_virtualenv
 	@echo "Updating pip"
-	@. ../venv/bin/activate; python3 -m pip install --upgrade pip==23.2.1 --require-virtualenv
+	@. ./venv/bin/activate; python3 -m pip install --upgrade pip==23.2.1 --require-virtualenv
 
 install_deps: update_pip
 	@echo "Installing dependencies"
-	@. ../venv/bin/activate; pip3 install --no-cache-dir --require-virtualenv -r ../requirements/requirements.txt
+	@. ./venv/bin/activate; pip3 install --no-cache-dir --require-virtualenv -r ./requirements/requirements.txt

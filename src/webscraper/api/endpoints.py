b1d4ec14820275810896374a56e9acf795a18276
csubhodeep <buks1019@gmail.com>
2023-09-12 20:11:40 +0200

    finalize read endpoint

--- src/webscraper/api/endpoints.py
+++ src/webscraper/api/endpoints.py
@@ -3,9 +3,15 @@ import os
 from fastapi import Depends
 from fastapi import FastAPI
 import sqlalchemy
-from webscraper.db.init_db import Base
+from webscraper.api.models import Part
+
+from webscraper.db.tables import Base
 from webscraper.db.init_db import get_engine
 from webscraper.utils.params import SQLALCHEMY_DATABASE_URL
+
+from typing import Any
+
+import pandas as pd
 
 app = FastAPI()
 
@@ -15,10 +21,20 @@ def get_db_engine() -> sqlalchemy.engine.Engine:
     # inject config during runtime
     url = os.getenv("SQLALCHEMY_DATABASE_URL", SQLALCHEMY_DATABASE_URL)
     db_engine = get_engine(url)
-    Base.metadata.create_all(bind=db_engine)
+    if not db_engine.has_table("parts"):
+        Base.metadata.create_all(db_engine)
     yield db_engine
 
 
-@app.get("/")
-def read_root(db_session: sqlalchemy.engine.Engine = Depends(get_db_engine)):
-    return {"msg": "Hello World"}
+@app.get("/", response_model=list[Part])
+async def read_root(
+    db_engine: sqlalchemy.engine.Engine = Depends(get_db_engine),
+    manufacturer: str | None = None,
+) -> Any:
+    if manufacturer is None:
+        query = "SELECT * FROM parts LIMIT 5"
+    else:
+        query = f"SELECT * FROM parts WHERE manufacturer = '{manufacturer}'"
+    df = pd.read_sql(query, db_engine)
+
+    return df.to_dict(orient="records")

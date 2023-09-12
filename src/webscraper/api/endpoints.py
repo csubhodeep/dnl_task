eb72d3d1fb334ed520c5cdbf405de1dd57afebb9
csubhodeep <buks1019@gmail.com>
2023-09-12 14:50:53 +0200

    fix db - fix Base - use Engine instead of Session

--- src/webscraper/api/endpoints.py
+++ src/webscraper/api/endpoints.py
@@ -1,24 +1,24 @@
+import os
+
 from fastapi import Depends
 from fastapi import FastAPI
-from sqlalchemy.orm import Session
+import sqlalchemy
 from webscraper.db.init_db import Base
-from webscraper.db.init_db import get_session
+from webscraper.db.init_db import get_engine
 from webscraper.utils.params import SQLALCHEMY_DATABASE_URL
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=get_session(SQLALCHEMY_DATABASE_URL).get_bind())
-
 
 # Dependency
-def get_db_sess():
-    db = get_session(SQLALCHEMY_DATABASE_URL)
-    try:
-        yield db
-    finally:
-        db.close()
+def get_db_engine() -> sqlalchemy.engine.Engine:
+    # inject config during runtime
+    url = os.getenv("SQLALCHEMY_DATABASE_URL", SQLALCHEMY_DATABASE_URL)
+    db_engine = get_engine(url)
+    Base.metadata.create_all(bind=db_engine)
+    yield db_engine
 
 
 @app.get("/")
-def read_root(db_sess: Session = Depends(get_db_sess)):
+def read_root(db_session: sqlalchemy.engine.Engine = Depends(get_db_engine)):
     return {"msg": "Hello World"}
--- src/webscraper/db/init_db.py
+++ src/webscraper/db/init_db.py
@@ -2,14 +2,11 @@ from __future__ import annotations
 
 import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
 
 
-def get_session(url: str) -> sqlalchemy.orm.session.Session:
-    engine = create_engine(url)
-    SessionLocal = sessionmaker(autocommit=False, bind=engine, autoflush=False)
-    return SessionLocal()
+def get_engine(url: str) -> sqlalchemy.engine.Engine:
+    return create_engine(url)

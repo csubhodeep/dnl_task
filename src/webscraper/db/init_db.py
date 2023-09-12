e02d0c40b0f2dcf5b4ab65d0e13b1c5b67de6a47
csubhodeep <buks1019@gmail.com>
2023-09-12 20:11:58 +0200

    remove declare base

--- src/webscraper/db/init_db.py
+++ src/webscraper/db/init_db.py
@@ -2,10 +2,6 @@ from __future__ import annotations
 
 import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-
-
-Base = declarative_base()
 
 
 def get_engine(url: str) -> sqlalchemy.engine.Engine:

c51f208e6d81d40a4a49dd3988030578ae8e1ba6
csubhodeep <buks1019@gmail.com>
2023-09-13 17:36:36 +0200

    finalize transformation script

--- src/webscraper/etl/transformation.py
+++ src/webscraper/etl/transformation.py
@@ -1,7 +1,25 @@
-from __future__ import annotations
-
 import pandas as pd
 
 
-def transform(raw_data: dict) -> pd.DataFrame:
+def transform(raw_data: list[dict[str, list[str]]]) -> pd.DataFrame:
     print("Transforming...")
+
+    records = []
+    # the below can be parallelized but skipping it for now
+    for entry in raw_data:
+        url, parts = entry.popitem()
+        _, info = url.split("/catalogue/")
+        manufacturer, category, model = info.split("/")
+        # FIXME: this could be unsafe
+        category = category.replace("%20", " ")
+        for part in parts:
+            records.append(
+                {
+                    "manufacturer": manufacturer,
+                    "category": category,
+                    "model": model,
+                    "part_number": part,
+                }
+            )
+
+    return pd.DataFrame(records)

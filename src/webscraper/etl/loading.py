731ac6f24d1f0ae9e2c95b2bc1d24909eb06b04d
csubhodeep <buks1019@gmail.com>
2023-09-13 18:57:02 +0200

    finalize loading script

--- src/webscraper/etl/loading.py
+++ src/webscraper/etl/loading.py
@@ -1,7 +1,13 @@
-from __future__ import annotations
-
 import pandas as pd
+
+import os
+
+from webscraper.db.init_db import get_engine
+from webscraper.utils.params import SQLALCHEMY_DATABASE_URL, TABLE_NAME
 
 
 def load(data: pd.DataFrame):
     print("Loading data to database...")
+    url = os.getenv("SQLALCHEMY_DATABASE_URL", SQLALCHEMY_DATABASE_URL)
+    db_engine = get_engine(url)
+    data.to_sql(TABLE_NAME, db_engine, if_exists="append", index=False)
--- tests/test_etl/test_loading.py
+++ tests/test_etl/test_loading.py
@@ -1,9 +1,10 @@
 from webscraper.utils.params import TABLE_NAME
+from webscraper.etl.loading import load
 
 
-def test_insert_records(db_engine, test_data_df):
-    test_data_df.to_sql(TABLE_NAME, db_engine, if_exists="append", index=False)
+def test_insert_records(db_engine, test_data_df, mock_env):
+    load(test_data_df)
 
-    assert db_engine.execute("SELECT COUNT(*) FROM parts").fetchone()[0] == len(
+    assert db_engine.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}").fetchone()[0] == len(
         test_data_df
     )

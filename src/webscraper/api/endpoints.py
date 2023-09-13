cf8c52013bd9427e6434872cc9171113f40204eb
csubhodeep <buks1019@gmail.com>
2023-09-13 18:51:37 +0200

    remove table creation from read-only endpoint

--- src/webscraper/api/endpoints.py
+++ src/webscraper/api/endpoints.py
@@ -6,7 +6,6 @@ import sqlalchemy
 from fastapi import Depends
 from fastapi import FastAPI
 from webscraper.api.models import Part
-from webscraper.db.tables import Base
 from webscraper.db.init_db import get_engine
 from webscraper.utils.params import SQLALCHEMY_DATABASE_URL
 
@@ -19,8 +18,6 @@ def get_db_engine() -> sqlalchemy.engine.Engine:
     # inject config during runtime
     url = os.getenv("SQLALCHEMY_DATABASE_URL", SQLALCHEMY_DATABASE_URL)
     db_engine = get_engine(url)
-    if not db_engine.has_table("parts"):
-        Base.metadata.create_all(db_engine)
     yield db_engine
 
 

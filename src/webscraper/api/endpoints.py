b5b2d825a044316c42ded27e67e527a6504a0679
csubhodeep <buks1019@gmail.com>
2023-09-14 16:21:27 +0200

    add doc strings

--- src/webscraper/api/endpoints.py
+++ src/webscraper/api/endpoints.py
@@ -7,7 +7,7 @@ from fastapi import Depends
 from fastapi import FastAPI
 from webscraper.api.models import Part
 from webscraper.db.init_db import get_engine
-from webscraper.utils.params import SQLALCHEMY_DATABASE_URL
+from webscraper.utils.params import SQLALCHEMY_DATABASE_URL, TABLE_NAME
 
 
 app = FastAPI()
@@ -15,6 +15,9 @@ app = FastAPI()
 
 # Dependency
 def get_db_engine() -> sqlalchemy.engine.Engine:
+    """This function returns a database engine.
+    :return: A database engine.
+    """
     # inject config during runtime
     url = os.getenv("SQLALCHEMY_DATABASE_URL", SQLALCHEMY_DATABASE_URL)
     db_engine = get_engine(url)
@@ -25,11 +28,35 @@ def get_db_engine() -> sqlalchemy.engine.Engine:
 async def read_data(
     db_engine: sqlalchemy.engine.Engine = Depends(get_db_engine),
     manufacturer: str | None = None,
+    model: str | None = None,
+    category: str | None = None,
 ) -> Any:
+    """This endpoint returns a list of parts from the database.
+    :param db_engine: The database engine.
+    :param manufacturer: The manufacturer of the parts.
+    :param model: The model of the parts.
+    :param category: The category of the parts.
+    :return: A list of parts in the form of
+        [
+            {
+                "manufacturer": str,
+                "model": str,
+                "category": str",
+                "part_number": str,
+            },
+            ...
+        ]
+    """
     if manufacturer is None:
-        query = "SELECT * FROM parts LIMIT 5"
+        query = f"SELECT * FROM {TABLE_NAME} LIMIT 5"
     else:
-        query = f"SELECT * FROM parts WHERE manufacturer = '{manufacturer}'"
+        query = f"""
+            SELECT * 
+            FROM {TABLE_NAME} 
+            WHERE manufacturer = '{manufacturer}'
+                AND model = '{model}'
+                AND category = '{category}'
+        """
     df = pd.read_sql(query, db_engine)
 
     return df.to_dict(orient="records")

f518f6d08efd0701a4667eddeee3dd8f50420e4a
csubhodeep <buks1019@gmail.com>
2023-09-14 19:29:10 +0200

    add example index for faster filtering on querying

--- src/webscraper/db/tables.py
+++ src/webscraper/db/tables.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sqlalchemy import Column
 from sqlalchemy import DateTime
 from sqlalchemy import Integer
+from sqlalchemy import Index
 from sqlalchemy import String
 from sqlalchemy.orm import declarative_base
 from webscraper.utils.params import TABLE_NAME
@@ -14,9 +15,12 @@ Base = declarative_base()
 class Parts(Base):  # type: ignore
     __tablename__ = TABLE_NAME
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, auto_increment=True)
     load_timestamp_utc = Column(DateTime, default=datetime.utcnow())
     manufacturer = Column(String(50), nullable=False)
     model = Column(String(50), nullable=False)
     category = Column(String(50), nullable=False)
     part_number = Column(String(50), nullable=False)
+
+    # More indexes can be added here, if needed.
+    __table_args__ = (Index("idx_manufacturer", "manufacturer"),)
